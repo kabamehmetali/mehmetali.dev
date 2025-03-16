70c9aa965bd09e54cc4ec919155edba9a8f44a43
kabamehmetali <mehmetalikabaa@icloud.com>
2025-03-16 07:59:59 -0400

    directly

--- server/server.js
+++ server/server.js
@@ -4,37 +4,57 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Absolute path to the 'public' folder
-const publicPath = path.resolve(__dirname, '../public');
+// Serve static files directly from public folder
+app.use(express.static(path.resolve(__dirname, '../public')));
 
-// Serve static assets directly from 'public'
-app.use(express.static(publicPath));
-
-// Explicit routes to serve HTML pages
-const pages = [
-  'index',
-  'home',
-  'about-me',
-  'resume',
-  'cover-letter',
-  'links',
-  'pages',
-  'credentials',
-  'blog',
-  'blog-post',
-  'projects',
-  'project-details'
-];
-
-pages.forEach(page => {
-  app.get(`/${page === 'index' ? '' : page}`, (req, res) => {
-    res.sendFile(path.join(publicPath, `${page}.html`));
-  });
+// Redirect root and friendly URLs explicitly to HTML files
+app.get(['/', '/home'], (req, res) => {
+  res.redirect('/index.html');
 });
 
-// 404 Handler
+app.get('/blog', (req, res) => {
+  res.redirect('/blog.html');
+});
+
+app.get('/about-me', (req, res) => {
+  res.redirect('/about-me.html');
+});
+
+app.get('/resume', (req, res) => {
+  res.redirect('/resume.html');
+});
+
+app.get('/cover-letter', (req, res) => {
+  res.redirect('/cover-letter.html');
+});
+
+app.get('/links', (req, res) => {
+  res.redirect('/links.html');
+});
+
+app.get('/pages', (req, res) => {
+  res.redirect('/pages.html');
+});
+
+app.get('/credentials', (req, res) => {
+  res.redirect('/credentials.html');
+});
+
+app.get('/blog-post', (req, res) => {
+  res.redirect('/blog-post.html');
+});
+
+app.get('/projects', (req, res) => {
+  res.redirect('/projects.html');
+});
+
+app.get('/project-details', (req, res) => {
+  res.redirect('/project-details.html');
+});
+
+// Proper 404 handling
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(publicPath, '404.html'));
+  res.status(404).sendFile(path.resolve(__dirname, '../public/404.html'));
 });
 
 // Start the server
