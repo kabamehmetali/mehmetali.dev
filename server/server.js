a5f026eac2aca8f95e3e99f93301a52b7802c211
kabamehmetali <mehmetalikabaa@icloud.com>
2025-03-16 07:37:56 -0400

    test

--- server/server.js
+++ server/server.js
@@ -8,60 +8,25 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
-// Existing routes
-app.get('/', (req, res) => {
+// Generic route handler (automatically serve HTML pages without extensions)
+app.get(['/', '/home'], (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/:page', (req, res, next) => {
+  const page = req.params.page;
+  const filePath = path.join(publicPath, `${page}.html`);
+  res.sendFile(filePath, (err) => {
+    if (err) next();
+  });
 });
 
-app.get('/about-me', (req, res) => {
-  res.sendFile(path.join(publicPath, 'about-me.html'));
-});
-
-app.get('/resume', (req, res) => {
-  res.sendFile(path.join(publicPath, 'resume.html'));
-});
-
-app.get('/cover-letter', (req, res) => {
-  res.sendFile(path.join(publicPath, 'cover-letter.html'));
-});
-
-app.get('/links', (req, res) => {
-  res.sendFile(path.join(publicPath, 'links.html'));
-});
-
-app.get('/pages', (req, res) => {
-  res.sendFile(path.join(publicPath, 'pages.html'));
-});
-
-app.get('/credentials', (req, res) => {
-  res.sendFile(path.join(publicPath, 'credentials.html'));
-});
-
-// Blog listing page
-app.get('/blog', (req, res) => {
-  res.sendFile(path.join(publicPath, 'blog.html'));
-});
-
-// Single blog post page
-app.get('/blog-post', (req, res) => {
-  res.sendFile(path.join(publicPath, 'blog-post.html'));
-});
-
-// NEW: Projects listing page
-app.get('/projects', (req, res) => {
-  res.sendFile(path.join(publicPath, 'projects.html'));
-});
-
-// NEW: Single project details page
-app.get('/project-details', (req, res) => {
-  res.sendFile(path.join(publicPath, 'project-details.html'));
+// Handle 404 - Not Found
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(publicPath, '404.html'));
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
